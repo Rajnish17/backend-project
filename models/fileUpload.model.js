8618bc1070e7fc83044e062b8373482412364f4d
Rajnish <RajnishAryan12@gmail.com>
2023-12-29 17:57:12 +0530

    fileUpload schema added

---
+++ models/fileUpload.model.js
@@ -0,0 +1,13 @@
+// models/Upload.js
+const mongoose = require('mongoose');
+
+const uploadSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
+const Upload = mongoose.model('Upload', uploadSchema);
+
+module.exports = Upload;
